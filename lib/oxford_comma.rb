6a569b7f0ad612da2850bbc4b528474664b64013
callie rogers <callie1013@gmail.com>
2021-05-07 02:53:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 2
+          array.join(" and ")
+      elsif array.length > 2
+          array.insert(-2, "and")
+          array[0..-1].join(", ")
+      else 
+          ['a'].join(' and ')
 end
+end 
