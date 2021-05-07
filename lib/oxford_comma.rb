adca91136010a84f374cae569eeaf38abc1fe64a
callie rogers <callie1013@gmail.com>
2021-05-07 03:00:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
-  if array.length == 2
-          array.join(" and ")
-      elsif array.length > 2
-          array.insert(-2, "and")
-          array[0..-1].join(", ")
-      else
-          ['a'].join(' and ')
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
 end 
